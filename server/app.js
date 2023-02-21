1cd1a0e7ce335af85418b3499507686db6567b94
Kim Young Min <kastori1990@gmail.com>
2023-02-21 15:12:12 +0900

    backend api 수정, 삭제 추가

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-let id = 2;
+
 const todoList = [
   {
     id: 1,
@@ -14,20 +14,51 @@ const todoList = [
   },
 ];
 
+//조회
 app.get("/api/todo", (req, res) => {
   res.json(todoList);
 });
 
+//추가
 app.post("/api/todo", (req, res) => {
-  console.log("들어왔니", req.body);
-  const { text, done } = req.body;
+  const { id, text, done } = req.body;
+
   todoList.push({
-    id: id++,
+    id,
     text,
     done,
   });
+
   return res.send("success");
 });
+
+//수정
+app.put("/api/todo/:id", (req, res) => {
+  const index = todoList.findIndex((t) => t.id === parseInt(req.params.id));
+
+  if (index === -1) {
+    return res.send("fail");
+  }
+
+  todoList[index]["text"] =
+    req.body.text === undefined ? todoList[index]["text"] : req.body.text;
+  todoList[index]["done"] =
+    req.body.done === undefined ? todoList[index]["done"] : req.body.done;
+
+  return res.send("success");
+});
+
+//삭제
+app.delete("/api/todo/:id", (req, res) => {
+  const index = todoList.findIndex((t) => t.id === parseInt(req.params.id));
+
+  if (index === -1) {
+    return res.send("fail");
+  }
+  todoList.splice(index, 1);
+  return res.send("success");
+});
+
 app.listen(4000, () => {
   console.log("server start!!");
 });
